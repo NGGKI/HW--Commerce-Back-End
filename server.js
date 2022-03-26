7c911fdc4b8f839e9d537744b920ab3177248bdc
NGGKI <kien.dv2012@gmail.com>
2022-03-25 20:41:52 -0700

    code.

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// import sequelize connection
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
+
+// sync sequelize models to the database, then turn on the server
+async function init() {
+  await require('./config/connection').sync()
+  app.listen(process.env.PORT || 3000)
+}
+
+init()
